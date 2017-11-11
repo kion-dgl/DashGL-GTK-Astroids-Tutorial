77d3579244c42254dd799377a19898040e3bedff
kion <kion@dashgl.com>
2017-11-11 23:30:15 +0900

    draw astroid

---
+++ 13_Draw_Astroid/shader/fragment.glsl
@@ -0,0 +1,17 @@
+#version 130
+
+varying vec2 texcoord;
+uniform sampler2D mytexture;
+
+void main(void) {
+
+    vec2 flipped_texcoord = vec2(texcoord.x, 1.0 - texcoord.y);
+    vec4 frag = texture2D(mytexture, flipped_texcoord);
+    
+    if(frag.z == 0) {
+        discard;
+    }
+    
+    gl_FragColor = frag;
+
+}
